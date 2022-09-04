5a16eef4a464528cfd311cb43628415d297b6978
Eitan Monsa <eitan357@gmail.com>
2022-09-04 14:15:27 +0300

    4/9/22 5

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const customersRouter = require("./routers/customersRouter");
 const productsRouter = require("./routers/productsRouter");
@@ -14,6 +15,12 @@ app.use(express.json());
 app.use("/api/customers", customersRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/purchases", purchasesRouter);
+
+app.use(express.static(path.join(__dirname, "appstore/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/appstore/build", "index.html"));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`API running`));
